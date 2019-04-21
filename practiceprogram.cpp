ce29a9747e445db817ee1e698130637751eb6649
SpayL <43752455+SpayL@users.noreply.github.com>
2019-04-21 22:36:36 +0530

    Update practiceprogram.cpp

--- practiceprogram.cpp
+++ practiceprogram.cpp
@@ -572,64 +572,71 @@ void printArray(int arr[], int n)
 
 //queue array implementation
 
-class Queue
-{
-    public:
-    int front, rear, size;
-    unsigned capacity;
-    int* array;
-};
+class Queue  
+{  
+    public: 
+    int front, rear, size;  
+    unsigned capacity;  
+    int* array;  
+};  
+  
+ 
+Queue* createQueue(unsigned capacity)  
+{  
+    Queue* queue = new Queue(); 
+    queue->capacity = capacity;  
+    queue->front = queue->size = 0;  
+    queue->rear = capacity - 1; // This is important, see the enqueue  
+    queue->array = new int[(queue->capacity * sizeof(int))];  
+    return queue;  
+}  
+  
+  
+int isFull(Queue* queue)  
+{ return (queue->size == queue->capacity); }  
+  
+ 
+int isEmpty(Queue* queue)  
+{ return (queue->size == 0); }  
+  
 
-Queue* createQueue(unsigned capacity)
-{
-    Queue* queue = new Queue();
-    queue->capacity = capacity;
-    queue->front = queue->size = 0;
-    queue->rear = capacity - 1; // This is important, see the enqueue
-    queue->array = new int[(queue->capacity * sizeof(int))];
-    return queue;
-}
+void enqueue(Queue* queue, int item)  
+{  
+    if (isFull(queue))  
+        return;  
+    queue->rear = (queue->rear + 1) % queue->capacity;  
+    queue->array[queue->rear] = item;  
+    queue->size = queue->size + 1;  
+    cout << item << " enqueued to queue\n";  
+}  
+  
+ 
+int dequeue(Queue* queue)  
+{  
+    if (isEmpty(queue))  
+        return INT_MIN;  
+    int item = queue->array[queue->front];  
+    queue->front = (queue->front + 1) % queue->capacity;  
+    queue->size = queue->size - 1;  
+    return item;  
+}  
+  
+  
+int front(Queue* queue)  
+{  
+    if (isEmpty(queue))  
+        return INT_MIN;  
+    return queue->array[queue->front];  
+}  
+   
+int rear(Queue* queue)  
+{  
+    if (isEmpty(queue))  
+        return INT_MIN;  
+    return queue->array[queue->rear];  
+}  
+  
 
-int isFull(Queue* queue)
-{ return (queue->size == queue->capacity); }
-
-int isEmpty(Queue* queue)
-{ return (queue->size == 0); }
-
-void enqueue(Queue* queue, int item)
-{
-    if (isFull(queue))
-        return;
-    queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->array[queue->rear] = item;
-    queue->size = queue->size + 1;
-    cout << item << " enqueued to queue\n";
-}
-
-
-int dequeue(Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    int item = queue->array[queue->front];
-    queue->front = (queue->front + 1) % queue->capacity;
-    queue->size = queue->size - 1;
-    return item;
-}
-
-int front(Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->front];
-}
-
-int rear(Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->rear];
-}
 
 //program for tree traversal
 
